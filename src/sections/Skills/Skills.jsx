4b96165c8bff12f9cb25858a4ef73b91294b55f9
Pritesh Dusane <priteshdusane0429@gmail.com>
2025-01-06 16:58:10 +0530

    skills_update

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,22 +13,38 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Reactjs" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-      </div>
-      <hr />
-      <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="SASS" />
+        <SkillList src={checkMarkIcon} skill="Bootstrap" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
-        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Context API" />
+        <SkillList src={checkMarkIcon} skill="React Query" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="RESTful APIs" />
+        <SkillList src={checkMarkIcon} skill="Axios" />
         <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="React Testing Library" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Webpack" />
+        <SkillList src={checkMarkIcon} skill="Babel" />
+        <SkillList src={checkMarkIcon} skill="NPM" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Agile/Scrum" />
+        <SkillList src={checkMarkIcon} skill="Performance Optimization" />
+        <SkillList src={checkMarkIcon} skill="Responsive Design" />
       </div>
     </section>
   );
